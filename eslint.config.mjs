56b3588667c2e350d5486e09b413b2f20053b1fa
jimmybatuhan <jimmybatuhan@gmail.com>
2025-03-05 20:25:42 +0800

    removed unused components and comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": [
-        "error", // or "error"
+        "error",
         {
           "caughtErrorsIgnorePattern": "^_"
         }
